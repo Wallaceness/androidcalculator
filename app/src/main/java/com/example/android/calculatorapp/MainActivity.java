6256b35f7869974d3136c789fb6f628433301804
Wallaceness <ncw232@gmail.com>
2020-01-23 13:26:12 -0500

    Functional (ugly) calculator app.

--- app/src/main/java/com/example/android/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/android/calculatorapp/MainActivity.java
@@ -14,14 +14,17 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    public String display="0";
+    private String display="0";
+    private String storedNumber;
+    private String storedOperation;
     private TextView calculatorScreen;
+    private boolean operationLast=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calculatorScreen= (TextView) findViewById(R.id.calc_screen);
+        calculatorScreen = (TextView) findViewById(R.id.calc_screen);
     }
 
     public void updateDisplay(View view){
@@ -29,7 +32,19 @@ public class MainActivity extends AppCompatActivity {
         display=(String) calculatorScreen.getText();
         Button b=(Button) view;
         String buttonText= b.getText().toString();
-        if (display.equals("0")){
+        if (operationLast){
+            storedNumber=display;
+            display=buttonText;
+            calculatorScreen.setText(display);
+            operationLast=false;
+        }
+        else if (buttonText.equals(".")){
+            if (display.indexOf('.')==-1){
+                display+=buttonText;
+                calculatorScreen.setText(display);
+            }
+        }
+        else if (display.equals("0")){
             Log.d(TAG, buttonText);
             display=buttonText;
             calculatorScreen.setText(display);
@@ -40,9 +55,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void add(){
-//        return a + b;
-        Log.d(TAG, "add: ");
+    public void AC(View view){
+        display="0";
+        calculatorScreen.setText(display);
+    }
+
+    public void operation(View view){
+        display=(String) calculatorScreen.getText();
+        Button b=(Button) view;
+        String buttonText= b.getText().toString();
+        storedOperation=buttonText;
+        operationLast=true;
+    }
+
+    public void onEquals(View view){
+        if (storedOperation!=null && !operationLast){
+            double storedValue= Double.parseDouble(storedNumber);
+            double displayValue=Double.parseDouble(display);
+            double result;
+            if (storedOperation.equals("+")){
+                result=add(storedValue, displayValue);
+            }
+            else if (storedOperation.equals("-")){
+                result=subtract(storedValue, displayValue);
+            }
+            else if (storedOperation.equals("x")){
+                result=multiply(storedValue, displayValue);
+            }
+            else if (storedOperation.equals("÷")){
+                result=divide(storedValue, displayValue);
+            }
+            else if (storedOperation.equals("^")){
+                result=power(storedValue, displayValue);
+            }
+            else if (storedOperation.equals("√")){
+                result=squareRoot(storedValue, displayValue);
+            }
+            else{
+                result=0;
+            }
+            display=Double.toString(result);
+            calculatorScreen.setText(display);
+            storedNumber=null;
+            storedOperation=null;
+        }
+    }
+
+    public double add(double a, double b){
+        return a + b;
     }
 
     protected double subtract(double a, double b){
@@ -61,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
         return Math.pow(number, exponent);
     }
 
-    protected double squareRoot(double number){
-        return Math.sqrt(number);
+    protected double squareRoot(double number, double exponent){
+        return Math.pow(number, 1/exponent);
     }
 }
